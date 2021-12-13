11ec32beb33b31fbcad710b70c4834287fa71b06
Suzanne Samoilova <suzelle@yandex.ru>
2021-12-13 22:06:51 +0300

    add input login and password

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="">
+        <h2 className="">
+        </h2>
+        <form className="" name="">
+          <input className="" id="login"
+                 type="text"
+                 name="login"
+                 placeholder="Логин"
+                 required
+          />
+          <input className="" id="password"
+                 type="text"
+                 name="password"
+                 placeholder="Пароль"
+                 required
+          />
+          <button className="" type="submit">
+          </button>
+        </form>
+      </div>
+
     </div>
   );
 }
