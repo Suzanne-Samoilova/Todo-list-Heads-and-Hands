c676a17fcb32d2716d58bcbedbd38e0a9c1dfe2e
Suzanne Samoilova <suzelle@yandex.ru>
2021-12-16 11:35:43 +0300

    feat: authorization, refactor: delete unnecessary files

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,14 +3,14 @@ import axios from "axios";
 import {useDispatch, useSelector} from "react-redux";
 
 function Form() {
+    // для авторизации
+    const [email, setEmail] = React.useState<string>();
+    const [password, setPassword] = React.useState<string>();
 
+    // для стора
     const dispatch = useDispatch()
     const auth = useSelector((state: any) => state.isAuthorized)
     const todo = useSelector((state: any) => state.todo)
-
-
-    const [email, setEmail] = React.useState<string>();
-    const [password, setPassword] = React.useState<string>();
 
     const handleEmail = (e:React.ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value)
@@ -22,26 +22,32 @@ function Form() {
 
 
     function handleSubmit(e:React.ChangeEvent<HTMLFormElement>) {
+        // без него ничего не отправляю (не получаю)
         e.preventDefault();
+
         axios.get(`http://localhost:3001/users?email=${email}&password=${password}`)
             .then(resp => {
                 if (resp.data.length) {
                     const userId = resp.data[0].id
                     console.log(resp, "Юзер найден!")
-                    axios.get(`http://localhost:3001/todo?user_id=${userId}`).then(
-                        resp => {
-                            dispatch({type: "login", payload: {userId: userId, todo: resp.data}})
-                        }
-                    ).catch(error =>
+                    axios.get(`http://localhost:3001/todo?user_id=${userId}`)
+                        .then(resp => {
+                            dispatch({
+                                type: "login",
+                                payload: {
+                                    userId: userId,
+                                    todo: resp.data}})
+                        })
+                        .catch(error =>
                         console.log('error:', error))
-
                     // document.location.href = 'http://stackoverflow.com'
                 } else {
                     console.log(resp, "Такого юзера нет!")
                 }
             })
             .catch(error =>
-                console.log('error:', error));
+                console.log('error:', error)
+            );
     }
 
 
@@ -77,9 +83,19 @@ function Form() {
                 </form>
             </div>
             }
+
             {auth &&
                 <div>Успешный логин {email}!
-                    <button className="authorization__button-save" onClick={() => dispatch({type: "logout", payload: {userId: undefined, todo: []}})}>Выйти</button>
+                    <button className="authorization__button-save" onClick={() =>
+                        dispatch({
+                            type: "logout",
+                            payload: {
+                                userId: undefined,
+                                todo: []
+                            }
+                        })}>
+                        Выйти
+                    </button>
                     {JSON.stringify(todo)}
                 </div>
             }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import {Provider} from "react-redux";
 import {createStore} from "redux";
 
@@ -15,10 +15,18 @@ const reducer = (state:any = defaultState, action: any) => {
     switch (action.type) {
         case "login":
             console.log(action.payload.userId, state)
-            return {...state, isAuthorized: true, userId: action.payload.userId, todo: action.payload.todo}
+            return {...state,
+                isAuthorized: true,
+                userId: action.payload.userId,
+                todo: action.payload.todo}
+
         case "logout":
             console.log(action.payload.userId, state)
-            return {...state, isAuthorized: false, userId: action.payload.userId, todo: action.payload.todo}
+            return {...state,
+                isAuthorized: false,
+                userId: action.payload.userId,
+                todo: action.payload.todo}
+
         default:
             return state
     }
