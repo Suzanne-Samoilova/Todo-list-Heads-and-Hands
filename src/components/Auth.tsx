13f203736343a674678e8f6fe1a24cd2c6c514b7
Suzanne Samoilova <suzelle@yandex.ru>
2021-12-27 13:54:56 +0300

    style: button disabled in auth-page

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -40,36 +40,44 @@ function Auth() {
 
 
     return (
-        <div className="authorization">
-            <h2 className="authorization__title">
-                Авторизация
-            </h2>
-            <form className="authorization__form" onSubmit={handleSubmit}>
-                <p className="authorization__input-title">E-mail:</p>
-                <input className="authorization__form-input" id="email"
-                       type="email"
-                       name="email"
-                       placeholder="Введите адрес эл.почты"
-                       required
-                       onChange={handleEmail}
-                       value={email}
-                />
-                <span className="authorization__form-error" id="email-error">Введите адрес эл.почты.</span>
+        <div className="auth">
+            <div className="authorization">
+                <h2 className="authorization__title">
+                    Авторизация
+                </h2>
+                <form className="authorization__form" onSubmit={handleSubmit}>
+                    <p className="authorization__input-title">E-mail:</p>
+                    <input className="authorization__form-input" id="email"
+                           type="email"
+                           name="email"
+                           placeholder="Введите адрес эл.почты"
+                           required
+                           onChange={handleEmail}
+                           value={email}
+                    />
+                    <span className="authorization__form-error" id="email-error">Введите адрес эл.почты.</span>
 
-                <p className="authorization__input-title">Пароль:</p>
-                <input className="authorization__form-input" id="password"
-                       type="text"
-                       name="password"
-                       placeholder="Введите пароль"
-                       required
-                       onChange={handlePassword}
-                       value={password}
-                />
-                <span className="authorization__form-error" id="password-error">Введите пароль.</span>
+                    <p className="authorization__input-title">Пароль:</p>
+                    <input className="authorization__form-input" id="password"
+                           type="text"
+                           name="password"
+                           placeholder="Введите пароль"
+                           required
+                           onChange={handlePassword}
+                           value={password}
+                    />
+                    <span className="authorization__form-error" id="password-error">Введите пароль.</span>
 
-                <button className="authorization__button-save" type="submit">Войти</button>
-            </form>
-            <button className="registration">Зарегистрироваться</button>
+                    <button className="authorization__button-save"
+                            type="submit"
+                            // disabled
+                    >Войти</button>
+                </form>
+
+                <button className="registration">Зарегистрироваться</button>
+            </div>
+
+            <button className="forgot-password">Забыли пароль?</button>
         </div>
     );
 }
